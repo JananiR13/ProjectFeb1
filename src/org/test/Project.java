f5948d7b2e8eada7318d9b8cda245b384eaa598b
JananiR13 <janani725@gmail.com>
2021-02-16 19:21:05 +0530

    completed

---
+++ src/org/test/Project.java
@@ -0,0 +1,25 @@
+package org.test;
+
+public class Project {
+	private void hello() {
+		// TODO Auto-generated method stub
+		
+		
+	int a=10,b=100;
+		
+		int c = a+b;
+		
+		System.out.println("addition: "+c);
+
+	}
+	public static void main(String[] args) {
+		
+		
+		Project s=new Project();
+		
+		System.out.println("Hello...");
+		
+		s.hello();
+}
+}
+
